30e279bdccff79924209276a945b5cd7d2126ab9
letsGoBharat <1993bharat.sharma@gmail.com>
2020-03-21 11:59:45 +0100

    Proper naming convention followed

---
+++ Restricted_Deep_Boltzman_Machine/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+# a = 1000
+# while a % 1000 == 0 and a <= 10000:
+#     print(a)
+#     a += 1000
+#
+#
+# x = np.arange(1000, 10001, 1000)
+# print(x)
+
+for i in range(10):
+    print(i)
